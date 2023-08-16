ddc60fd7d4ce5ead8e4a79cc200b52b705fbb6b8
alexandereversbusch <abusch419@gmail.com>
2023-08-16 01:47:13 -0400

    Random petal lengths every time.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use nannou::prelude::*;
+use rand::Rng;
 
 fn main() {
     nannou::app(model)
@@ -22,8 +23,8 @@ fn model(app: &App) -> Model {
     }
 }
 
-fn update(_app: &App, model: &mut Model, update: Update) {
-    model.flower.update(update.since_start.as_secs_f32());
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    model.flower.update();
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -35,54 +36,56 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
 struct AnimatedFlower {
     petal_rotation: f32,
-    rotation_speed: f32, // Speed at which the flower rotates
-    elapsed_time: f32,   // Time since the animation started
+    petal_widths: [f32; 5],
+    petal_heights: [f32; 5],
 }
 
 impl AnimatedFlower {
     pub fn new() -> Self {
+        let mut rng = rand::thread_rng();
+        let petal_widths: [f32; 5] = [
+            rng.gen_range(60.0..120.0),
+            rng.gen_range(60.0..120.0),
+            rng.gen_range(60.0..120.0),
+            rng.gen_range(60.0..120.0),
+            rng.gen_range(60.0..120.0),
+        ];
+        let petal_heights: [f32; 5] = [
+            rng.gen_range(100.0..200.0),
+            rng.gen_range(100.0..200.0),
+            rng.gen_range(100.0..200.0),
+            rng.gen_range(100.0..200.0),
+            rng.gen_range(100.0..200.0),
+        ];
+
         AnimatedFlower {
             petal_rotation: 0.0,
-            rotation_speed: 0.02, // Initial rotation speed
-            elapsed_time: 0.0,
+            petal_widths,
+            petal_heights,
         }
     }
 
     pub fn draw(&self, draw: &Draw) {
-        let colors = [RED, BLUE, YELLOW, GREEN, CYAN]; // Colors for the petals
-    
+        let colors = [RED, BLUE, YELLOW, GREEN, CYAN];
+
         for i in 0..5 {
-            // Calculate the position for each petal
             let x_pos = (i as f32 * 72.0).to_radians().cos() * 75.0;
             let y_pos = (i as f32 * 72.0).to_radians().sin() * 75.0;
-    
-            // Draw the petal as an ellipse (oval shape)
+
             draw.ellipse()
                 .color(colors[i])
-                .w_h(80.0, 150.0) // Width and height of the oval petal
+                .w_h(self.petal_widths[i], self.petal_heights[i])
                 .rotate(self.petal_rotation + (i as f32 * 72.0).to_radians())
                 .x_y(x_pos, y_pos);
         }
-    
-        // Draw the center of the flower on top of the petals
+
         draw.ellipse()
-            .color(DARKGREEN)
+            .color(DARKORANGE)
             .w_h(50.0, 50.0)
             .finish();
     }
 
-    pub fn update(&mut self, elapsed: f32) {
-        self.elapsed_time += elapsed;
-    
-        // Spin at the same speed for the desired duration
-        if self.elapsed_time < 5.0 {
-            self.petal_rotation += self.rotation_speed;
-        } else {
-            // After the desired duration, gradually reduce the rotation speed to make the flower stop
-            if self.rotation_speed > 0.001 { // Adjust this threshold to control the stopping duration
-                self.rotation_speed -= 0.00005;
-                self.petal_rotation += self.rotation_speed;
-            }
-        }
+    pub fn update(&mut self) {
+        self.petal_rotation += 0.01;
     }
 }
